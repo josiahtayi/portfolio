66e9e7a9530a0b03f675ae76c4a5c5360e41b582
josiahtayi <davidtayi19@gmail.com>
2025-02-27 21:49:08 +1100

    Initial commit - Portfolio website

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files (CSS, images, etc.)
+app.use(express.static("public"));
+
+// Set EJS as the templating engine
+app.set("view engine", "ejs");
+
+// Routes
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home", name: "Josiah Tayi" });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about", { title: "About Me" });
+});
+
+app.get("/projects", (req, res) => {
+    res.render("projects", { title: "Projects" });
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact", { title: "Contact Me" });
+});
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/contact", (req, res) => {
+    console.log(req.body);
+    res.send("Thank you for reaching out!");
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
